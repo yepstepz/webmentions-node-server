f987c08bf8af2a1e7a4aad8f17bebec8e4bd8a76
yepstepz <prettyoptimist@gmail.com>
2023-12-23 18:32:26 +0300

    feat(*): add railway

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,10 @@
 import { fastify } from '../routes/index.js'
 import '../../dot-env.js';
 
+const port = process.env.PORT || 3030;
+
 // Run the server!
-fastify.listen({ port: 3030 }, function (err, address) {
+fastify.listen({ port, host: '0.0.0.0' }, function (err, address) {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
